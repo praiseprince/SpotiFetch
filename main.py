27b6d24a2d9989be995153a17b32c1250efc962f
praiseprince <praiseprince35@gmail.com>
2023-09-01 06:14:49 -0400

    testover

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app=Flask(__name__)
 
 @app.route('/', methods =["GET", "POST"])
 def home():
-    #if request.method == "POST":
-        #url=request.form.get("spotify_url")
-        #dfile=SpotiFetch.execute(url)
-        #if dfile and os.path.exists(dfile):
-            #return send_file(dfile, as_attachment=True)
-    #SpotiFetch.delete_folder_contents(SpotiFetch.path)
+    if request.method == "POST":
+        url=request.form.get("spotify_url")
+        dfile=SpotiFetch.execute(url)
+        if dfile and os.path.exists(dfile):
+            return send_file(dfile, as_attachment=True)
+    SpotiFetch.delete_folder_contents(SpotiFetch.path)
     return render_template('index.html')
 
 if __name__=='__main__':
